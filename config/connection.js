f46c26c0296e1d5a75784319812cf4086825e293
Brandon U <unzueta.brandon@gmai.com>
2019-08-28 18:21:44 -0700

    fixing heroku

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
     var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
-        host: "localhost",
+        host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
         user: "root",
         password: "root",
